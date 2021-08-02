26e3f1d2b29584f7a7374bb14d025036d9ac7cb5
NatiTsegaye <86118205+NatiTsegaye@users.noreply.github.com>
2021-08-02 18:44:40 +0300

    Update __init__.py

--- Main/__init__.py
+++ Main/__init__.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from flask_restful import Api
 import os
-from Routes.routes import initialize_routes
+from Main.Routes.routes import initialize_routes
 from flask_cors import CORS
 
 # Place where app is defined
@@ -16,7 +16,3 @@ CORS(app, resources={r'/*':{"origins": "*"}},allow_headers=[
 
 initialize_routes(api)
 
-
-if __name__ == '__main__':
-    # Running app in debug mode
-    app.run(debug=True)
